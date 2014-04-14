38438a3e71e3661e2fc0095426582317e8063db1
Torbjørn Langland <tl_tl_tl80@hotmail.com>
2014-04-14 21:48:03 +0200

    Users can now sign up or off activities. Next: Friends listing

--- Implementasjon/ActivityPortal/src/java/Controller/CooperationBean.java
+++ Implementasjon/ActivityPortal/src/java/Controller/CooperationBean.java
@@ -6,7 +6,7 @@
 
 package Controller;
 import ProblemDomainModel.*;
-
+import Database.*;
 
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -17,8 +17,8 @@ import javax.inject.Inject;
  *
  * @author Sygnious
  * 
- * Purpose is to offer methods for cooperation between the User-objects and
- * Activity-objects since UserBean and ActivityBean are independent. 
+ * Purpose is to offer methods that requires data from both ActivityBean 
+ * and UserBean
  */
 @Named(value = "coopB")
 @SessionScoped
@@ -26,6 +26,7 @@ public class CooperationBean implements Serializable {
 
     @Inject private ActivityBean actBean;
     @Inject private UserBean userBean;
+    boolean isRendered;
     
     /**
      * Creates a new instance of CooperationBean
@@ -33,9 +34,53 @@ public class CooperationBean implements Serializable {
     public CooperationBean() {
     }
     
-    // Used for rendering between "Sign up" and "Cancel Sign up" - buttons
+    // Used for rendering between "Sign up" and "Cancel Sign up" - buttons.
     public boolean findParticipationOnUserID(){
         return (userBean.getSingleUser().participatesInActivity(actBean.getSingleAct().getActivityId()));
+    }
+    
+    // The get-method used for the rendering
+    public boolean getIsRendered(){
+        return isRendered = findParticipationOnUserID();
+    }
+    
+    // Methods for signing User to or from activities.
+    // The incorrect method should never be in display on the webpages,
+    // by rendering with the findParticipationOnUserID-method.
+    // Still, they will also be used internal in method for double control.
+    // Methods should update both database and both user's list of participated
+    // activites, sorted by date.
+    
+    // Note: User's list of activities is refreshed by reloading from database
+    // Should ideally be implemented by adding seperate activity to own list,
+    // and sort by using date comparison.
+    // Use of database reloading is currently selected in order to save
+    // implementation time.
+    public String signUserOnActivity() throws Exception{
+        // Control: Make sure user does NOT already participate
+        if (findParticipationOnUserID()){
+            return "error";
+        }
+        // Store user to activity, in database table activity_person:
+        Storer.storeUserToActivity(userBean.getSingleUser(), actBean.getSingleAct());
+        // Update user's activity list in class:
+        userBean.setSingleUser(Loader.loadSingleUserOnID(userBean.getSingleUser().getUserId()));
+        
+        return "activitySignedOn";
+    }
+    
+    // Using word "cancel" instead of "sign" in order to avoid confusion.
+    public String cancelUserFromActivity() throws Exception{ 
+        // Control: Make sure user DOES participate
+        if (!findParticipationOnUserID()){
+            return "error";
+        }
+        // Store user to activity, in database table activity_person:
+        Storer.deleteUserFromActivity(userBean.getSingleUser(), actBean.getSingleAct());
+        // Update user's activity list in class:
+        userBean.setSingleUser(Loader.loadSingleUserOnID(userBean.getSingleUser().getUserId()));
+        
+        return "activityCancelled";
     }
     
     
--- Implementasjon/ActivityPortal/test/Controller/CooperationBeanTest.java
+++ Implementasjon/ActivityPortal/test/Controller/CooperationBeanTest.java
@@ -8,6 +8,7 @@ package Controller;
 
 import Database.*;
 import ProblemDomainModel.*;
+import java.util.GregorianCalendar;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -64,7 +65,7 @@ public class CooperationBeanTest {
         System.out.println("findParticipationOnUserID");
         //Set up
         Activity testAct1 = new Activity(1, "TestAct1", null, 1);
-        Activity testAct2 = new Activity(99, "TestAct1", null, 1);
+        Activity testAct2 = new Activity(99, "TestAct2", null, 1);
         User user = new User(1, null, null, 1, null, null);
         user.insertActivity(new Activity(1, "MyAct1", null, 1));
         user.insertActivity(new Activity(4, "MyAct2", null, 1));
@@ -84,6 +85,92 @@ public class CooperationBeanTest {
         
         assertEquals("result should be false when user does not participate in the current activity", false, result);
         
+    }
+    
+    // StorerTest tests the database methods themselves, the methods connected to
+    // the CooperationBean which invoke storing will be tested for internal
+    // lists size, contents and return strings.
+    @Test
+    public void testSignUserOnActivity() throws Exception{
+        System.out.println("signUserOnActivity");
+        // Set up
+        prepareUserID2();
+        Activity testAct = new Activity(7, "TestAct1", null, 1); //Activity 7 comes before 8 in database
+        testAct.setDate(new GregorianCalendar(2014, 3, 2)); // First day after Loader demodate
+        instanceActB.setSingleAct(testAct); 
+
+        // Test 1: Adding new activity:
+        // Execute
+        String result = instance.signUserOnActivity();
+        int newSize = instanceUserB.getSingleUser().getPartActs().size();
+        assertEquals("resulting string was wrong", "activitySignedOn", result);
+        // Testing difference between old and new size, the static number may vary depending on implementation.
+        assertEquals("resulting size in participated activity list is not increased", 5, newSize);
+        Activity firstAct = instanceUserB.getSingleUser().getPartActs().get(0); //Should be first activty by given date
+        assertEquals("added activity found in list has wrong ID", 7, firstAct.getActivityId());
+        
+        // Test 2 Adding already added activity, should cause error and no update
+        // Execute
+        result = instance.signUserOnActivity();
+        newSize = instanceUserB.getSingleUser().getPartActs().size();
+        assertEquals("resulting string was wrong", "error", result);
+        assertEquals("resulting size in participated activity list has not remained unchanged", 5, newSize);
+    }
+    
+    @Test
+    public void testCancelUserFromActivity() throws Exception{
+        System.out.println("cancelUserFromActivity");
+        
+        // Set up
+        prepareUserID2();
+        // Removing activity with ID 8 from user (and thus database).
+        
+        // Test 1: Remove existing activity (ID 8),
+        String result = instance.cancelUserFromActivity();
+        int newSize = instanceUserB.getSingleUser().getPartActs().size();
+        assertEquals("resulting string was wrong", "activityCancelled", result);
+        // Testing difference between old and new size, the static number may vary depending on implementation.
+        assertEquals("resulting size in participated activity list is not decreased", 3, newSize);
+        Activity firstAct = instanceUserB.getSingleUser().getPartActs().get(0); //First activity removed, replaced by 2nd.
+        assertEquals("second activity moved to first index in list has wrong id", 1, firstAct.getActivityId());
+        
+        // Test 2 Removing already removed activity with ID 8, should cause error and no uptdate
+        // Execute
+        result = instance.cancelUserFromActivity();
+        newSize = instanceUserB.getSingleUser().getPartActs().size();
+        assertEquals("resulting string was wrong", "error", result);
+        assertEquals("resulting size in participated activity list has not remained unchanged", 3, newSize);
+    }
+    
+    // Helping methods for tests that are using userID2
+    private void prepareUserID2(){
+        // Goal: Prepare user ID 2 with contents for activity list based on
+        // contents from database. Only ID-numbers are essential and relevant.
+        // The order in database, based on date, are 8, 1, 2, 5.
+        // Warning: Contents may be overwritten with data from database
+        // depending on how CooperationBean implements methods for 
+        // refreshing and sorting user's list of participated activities
+        // Test only ID's from contents.
+        
+        // Dates may be overwritten with data from database, but if final 
+        // implementation only invokes store to database without reload,
+        // testing dates must be added for tests to succeed.
+        
+        User testUser = new User(2, null, null, 1, null, null);
+        Activity testAct = new Activity(8, "TestAct1", null, 1);
+        testAct.setDate(new GregorianCalendar(2014, 3, 3));
+        testUser.insertActivity(testAct); 
+        instanceUserB.setSingleUser(testUser);
+        instanceActB.setSingleAct(testAct); //Having the first activity as current activity
+        testAct = new Activity(1, "TestAct2", null, 1);
+        testAct.setDate(new GregorianCalendar(2014, 3, 4));
+        testUser.insertActivity(testAct);
+        testAct = new Activity(2, "TestAct3", null, 1);
+        testAct.setDate(new GregorianCalendar(2014, 3, 5));
+        testUser.insertActivity(testAct);
+        testAct = new Activity(5, "TestAct4", null, 1);
+        testAct.setDate(new GregorianCalendar(2014, 3, 6));
+        testUser.insertActivity(testAct);
         
     }
     
